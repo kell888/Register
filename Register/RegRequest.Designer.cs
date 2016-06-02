b0a7709fc11ee17e6fc8e7953112a700f7d94a7d
kell888 <421979530@qq.com>
2016-06-02 20:06:45 +0800

    2016-06-02

---
+++ Register/RegRequest.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Register
+{
+    partial class RegRequest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegRequest));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(59, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "确定注册";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(183, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "以后再说";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "用户姓名/单位名";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Blue;
+            this.textBox1.Location = new System.Drawing.Point(119, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.ForeColor = System.Drawing.Color.Blue;
+            this.textBox2.Location = new System.Drawing.Point(119, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "（未知）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "软件/网站名称";
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.Color.Blue;
+            this.textBox3.Location = new System.Drawing.Point(119, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 21);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "接收注册码Email";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(33, 63);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 16);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "手动填写软件名称";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // RegRequest
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(315, 245);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RegRequest";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "注册申请";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Register/RegRequest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace Register
+{
+    public partial class RegRequest : Form
+    {
+        public RegRequest()
+        {
+            InitializeComponent();
+        }
+
+        public string SoftWareName = "";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Trim() == "")
+            {
+                MessageBox.Show("软件/网站名称 不能为空，获取软件/网站名称失败！");
+                return;
+            }
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("用户姓名/单位名 不能为空！");
+                return;
+            }
+            if (textBox3.Text.Trim() == "")
+            {
+                MessageBox.Show("接收注册码Email 不能为空！");
+                return;
+            }
+            //判断接收注册码Email的合法性
+            //Regex reg = new Regex(@"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
+            if (!Regex.IsMatch(textBox3.Text.Trim(), @"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+            {
+                MessageBox.Show("接收注册码Email不合法，请重新输入！");
+                return;
+            }
+            ComputerInfo.Computer com = ComputerInfo.Computer.Instance();
+            string HardDiskSN = com.DiskPhysicalSN;
+            string userName = textBox1.Text.Trim();
+            string softWareName = textBox2.Text.Trim();
+            string receiveEmail = textBox3.Text.Trim();
+            //发送邮件
+            try
+            {
+                string from = "wonderful-abc@163.com";//发送人必须为163邮件地址
+                string to = "wonderful-abc@163.com";//邮件接收地址
+                string title = "软件客户注册";
+                string mailContent = "\n来自 " + userName + " 的软件 " + softWareName + " 需要注册，其注册申请码为 [" + HardDiskSN + "] 。\n请及时回复其注册码，回复地址为：" + receiveEmail;
+                MailMessage mm = new MailMessage(from, to, title, mailContent);
+                mm.SubjectEncoding = System.Text.Encoding.UTF8;
+                mm.BodyEncoding = System.Text.Encoding.UTF8;
+                
+                //"为什么Smtp类的Server属性不提供口令验证";
+                //mm.Attachments.Add(new MailAttachment("c:\\test.txt"));
+                //Attachments实现IList接口，可以添加多个附件
+                SmtpClient sc = new SmtpClient("smtp.163.com", 25);//pop.163.com
+                sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //sc.UseDefaultCredentials = true;
+                //sc.Credentials = CredentialCache.DefaultNetworkCredentials;
+                sc.Credentials = new System.Net.NetworkCredential("wonderful-abc", "11805129837159");//不可或缺，必须为发送人邮箱的登录帐号和密码
+                sc.Send(mm);
+                //SmtpMail.SmtpServer = "webmail.careland.com.cn";
+                //设置发送邮件的Smtp服务器，目前没有提供需要身份验证的服务器的登陆方法
+                //比如163.net,163.com都不行，目前我的是我公司的SMTP服务器
+                //看看大伙有没有更好的办法
+                //SmtpMail.Send(mm);
+                mm.Dispose();
+                MessageBox.Show("已经成功提交注册信息！\n请您留意在电子邮箱 " + receiveEmail + " 里查收注册码！\n谢谢您的支持！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (SoftWareName != "")
+            {
+                textBox2.Text = SoftWareName;
+            }
+            else
+            {
+                MessageBox.Show("自动获取软件名称失败，请手动填写！");
+                checkBox1.Checked = true;
+                textBox2.Enabled = true;
+                textBox2.Focus();
+                textBox2.SelectAll();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox2.Enabled = true;
+                textBox2.Focus();
+                textBox2.SelectAll();
+            }
+            else
+            {
+                textBox2.Enabled = false;
+            }
+        }
+    }
+}
